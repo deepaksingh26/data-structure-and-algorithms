897991004937643a571bf14e108201fd53b98e4b
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-07 11:29:32 +0530

    Time: 8 ms (9.49%), Space: 8.3 MB (24.64%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+       map<int,int>mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mpp[nums[i]]++;
+        }
+        nums.clear();
+        for(auto it:mpp)
+        {
+            int sec=it.second;
+            for(int i=0;i<sec;i++)
+            {
+                nums.push_back(it.first);
+            }
+        }
     }
 };
