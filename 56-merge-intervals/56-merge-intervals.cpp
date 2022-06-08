c6811617bc79a77042269011add71076ddb05a10
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-08 12:39:13 +0530

    Time: 36 ms (94.21%), Space: 19.2 MB (46.54%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -7,7 +7,7 @@ public:
         int end=in[0][1];
         for(int i=1;i<in.size();i++)
         {
-            if(in[i][0]<=end&&in[i][1]>=start)
+            if(in[i][0]<=end)
             {
                 if(in[i][0]<start)
                 start=in[i][0];
