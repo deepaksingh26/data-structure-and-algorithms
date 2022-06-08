d9dac9c733cc18e6515c8672438eb12838ce86cc
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-08 11:50:07 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.05%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,12 +5,12 @@ public:
         {
             for(int j=0;j<i;j++)
             {
-                swap(mat[i][j],mat[j][i]);
+                swap(mat[i][j],mat[j][i]);//transpose the matrix
             }
         }
         for(int i=0;i<mat.size();i++)
         {
-            reverse(mat[i].begin(),mat[i].end());
+            reverse(mat[i].begin(),mat[i].end());//then reverse the matrix 
         }
     }
 };
