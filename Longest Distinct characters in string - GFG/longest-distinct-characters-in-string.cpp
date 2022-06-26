44ce20fc5a3bb50cdab19fe844a561934c70439c
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-26 17:18:33 +0530

    Added solution - LeetHub

--- Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
+++ Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
@@ -17,34 +17,22 @@ int main()
 // } Driver Code Ends
 
 
-int longestSubstrDistinctChars (string str)
+int longestSubstrDistinctChars (string S)
 {
-      int maxans = 0;
-  unordered_set < int > set;
-  int l = 0;
-  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-  {
-    if (set.find(str[r]) != set.end()) //if duplicate element is found
+    int l=0;
+    int maxi=0;
+    unordered_set<int>st;
+    for(int r=0;r<S.length();r++)
     {
-      while (l < r && set.find(str[r]) != set.end()) {
-        set.erase(str[l]);
-        l++;
-      }
+        if(st.find(S[r])!=st.end()){
+           while(l<r && st.find(S[r])!=st.end())
+           {
+                st.erase(S[l]);
+                l++;
+           }
+        }
+        st.insert(S[r]);
+        maxi=max(maxi,r-l+1);
     }
-    set.insert(str[r]);
-    maxans = max(maxans, r - l + 1);
-  }
-  return maxans;
-    // int l=0;int maxi=0;
-    // unordered_set<int>st;
-    // for(int r=0;r<S.length();r++)
-    // {
-    //     if(st.find(S[r])!=st.end()){
-    //         st.erase(S[l]);
-    //         l++;
-    //     }
-    //     st.insert(S[r]);
-    //     maxi=max(maxi,r-l+1);
-    // }
-    // return maxi;
+    return maxi;
 }
