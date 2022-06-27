1723a19d2cab4d447ff23004d3c48f339ed578a4
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-27 10:10:25 +0530

    Time: 52 ms (21.05%), Space: 10.9 MB (22.88%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     
-int solve(string str) {
-  int maxans = 0;
-  unordered_set < int > set;
-  int l = 0;
-  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-  {
-    if (set.find(str[r]) != set.end()) //if duplicate element is found
-    {
-      while (l < r && set.find(str[r]) != set.end()) {
-        set.erase(str[l]);
-        l++;
-      }
-    }
-    set.insert(str[r]);
-    maxans = max(maxans, r - l + 1);
-  }
-  return maxans;
-}
+
     int lengthOfLongestSubstring(string s) {
-    //    int maxi=0;
-    //     unordered_set<int>st;
-    //     int l=0;
-    //     for(int r=0;r<s.length();r++)
-    //     {
-    //         if(st.find(s[r])!=st.end())
-    //         {
-    //             while(l<r && st.find(s[r])!=st.end())
-    //             {
-    //                 st.erase(s[l]);
-     //                l++
-    //             }
-    //         }
-    //         st.insert(s[r]);
-    //         maxi=max(maxi,r-l+1);
-    //     }
-    //  return maxi;
-        return solve(s);
-    }
+       int maxi=0;
+        unordered_set<int>st;
+        int l=0;
+        for(int r=0;r<s.length();r++)
+        {
+            if(st.find(s[r])!=st.end())
+            {
+                while(l<r && st.find(s[r])!=st.end())
+                {
+                    st.erase(s[l]);
+                    l++;
+                }
+            }
+            st.insert(s[r]);
+            maxi=max(maxi,r-l+1);
+        }
+     return maxi;
+    }    
 };
