2048cf091e0177910e76bc543c02d0386dc4eb65
deepak <81978223+deepaksingh26@users.noreply.github.com>
2022-06-30 20:04:08 +0530

    Time: 8 ms (84.87%), Space: 7.6 MB (73.46%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,18 +9,24 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head)
-    { 
-        if(!head||head->next==head)
-            return head;
-      unordered_set<ListNode *>st;
-      ListNode *front=head;
-      while(front)
-      {
-          if(st.find(front)!=st.end())
-              return front;
-          st.insert(front);
-          front=front->next;
-      }
-      return nullptr;
+    {if (head == NULL || head->next == NULL)
+            return NULL;
+    
+        ListNode *slow  = head;
+        ListNode *fast  = head;
+        ListNode *entry = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {                      // there is a cycle
+                while(slow != entry) {               // found the entry location
+                    slow  = slow->next;
+                    entry = entry->next;
+                }
+                return entry;
+            }
+        }
+        return NULL;      
     }
 };
